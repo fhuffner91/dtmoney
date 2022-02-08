961fe2399a3082168eb4dcbc40a297b602e24711
Felipe Huffner <felipeqh.1991@gmail.com>
2022-02-08 18:04:24 -0300

    Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,41 @@
+import incomeImg from '../../assets/Entradas.svg'
+import outcomeImg from '../../assets/Saídas.svg'
+import totalImg from '../../assets/Total.svg'
+import { Container } from "./styles";
+
+export function Summary(){
+    return(
+     
+        <Container>
+           <div>
+           <header>
+               <p>
+                 Entradas
+               </p>
+               <img src={incomeImg} alt="Entradas" />  
+            </header>
+            <strong>R$1000,00</strong>
+           </div> 
+           <div>
+           <header>
+               <p>
+                 Saídas
+               </p>
+               <img src={outcomeImg} alt="Saídas" />  
+            </header>
+            <strong>- R$500,00</strong>
+           </div> 
+           <div className="highlight-background">
+           <header>
+               <p>
+                 Total
+               </p>
+               <img src={totalImg} alt="total" />  
+            </header>
+            <strong>R$500,00</strong>
+           </div> 
+        </Container>
+        
+
+    );
+}
